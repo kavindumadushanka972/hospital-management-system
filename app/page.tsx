c9054b5efd852dbaf994866a3a69c40f03a89fa1
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2024-10-14 14:42:04 +0530

    Feat: admin passcode verification

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import PatientForm from '@/components/forms/PatientForm';
+import PasskeyModal from '@/components/PasskeyModal';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | Passkey Modal */}
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -23,19 +27,19 @@ export default function Home() {
               © 2024 CarePulse
             </p>
 
-            <Link href="/?admin=true" className='text-green-500'>
+            <Link href="/?admin=true" className="text-green-500">
               Admin
             </Link>
           </div>
         </div>
       </section>
 
-      <Image 
+      <Image
         src="/assets/images/onboarding-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className='side-img max-w-[50%]'
+        className="side-img max-w-[50%]"
       />
     </div>
   );
